be44460347af70a13a4a6ebf285e99ad64db9b4e
joyapple <124541823+joyapple@users.noreply.github.com>
2023-03-18 10:33:11 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -8,7 +8,7 @@ export const setting = {
 }
 
 export const message = `
-- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://s1.ax1x.com/2023/03/18/ppJZivD.png)，但不接受任何付费功能请求。
+- 如果本项目对你有所帮助，可以给我的小狗辣条 [买点零食](https://s1.ax1x.com/2023/03/18/ppJZivD.png)，但不接受任何付费功能请求。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
